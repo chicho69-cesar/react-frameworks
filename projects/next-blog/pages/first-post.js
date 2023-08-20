7d16e74c8cd3d1a00f303c01dbef4a77aad9b7e4
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-08-20 08:36:17 -0600

    feat(projects/next-blog): Finish all blog components and pages

---
+++ projects/next-blog/pages/first-post.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Link from 'next/link'
+// import Script from 'next/script'
+
+import Layout from 'components/layout'
+
+export default function FirstPostPage() {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+
+        {/* <Script
+          src='https://connect.facebook.net/en_US/sdk.js'
+          strategy='lazyOnload'
+          onLoad={() => {
+            console.log('script loaded correctly, window.FB has been populated')
+          }}
+        /> */}
+
+        <h1>
+          First post
+        </h1>
+
+        <Link href='/'>Back to home</Link>
+      </Layout>
+    </>
+  )
+}
