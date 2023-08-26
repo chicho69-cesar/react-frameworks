fdf7cda51817760d871bafdf49cdd63b7e4a0dba
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-08-26 11:07:09 -0600

    fix(projects/remix-jokes-app): Fix an error on singleton prisma db instance

--- projects/remix-jokes-app/app/utils/singleton.server.ts
+++ projects/remix-jokes-app/app/utils/singleton.server.ts
@@ -1,11 +1,11 @@
 export function singleton<Value>(
-  name: string, 
-  valueFactory: () => Value,
+  name: string,
+  valueFactory: () => Value
 ): Value {
   const g = global as any
 
   g.__singletons ??= {}
-  g.__singletons[name] ?? valueFactory()
+  g.__singletons[name] ??= valueFactory()
 
   return g.__singletons[name]
 }
