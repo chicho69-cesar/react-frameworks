2566c5cfe543631edf009df5a8f80b3d3b981bd7
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-09-07 23:58:31 -0600

    feat(projects/gatsby-site): Add render hero images on blog posts

--- projects/gatsby-site/gatsby-config.js
+++ projects/gatsby-site/gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
     },
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
--- projects/gatsby-site/src/pages/blog/{mdx.frontmatter__slug}.jsx
+++ projects/gatsby-site/src/pages/blog/{mdx.frontmatter__slug}.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+// import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 import Layout from '@/components/Layout'
 import Seo from '@/components/Seo'
@@ -10,15 +11,34 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_text
+        hero_image_credit_link
+        hero_image
       }
     }
   }
 `
 
 function BlogPostPage({ data, children }) {
+  // const image = getImage(data.mdx.frontmatter.hero_image)
+  const image = data.mdx.frontmatter.hero_image
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+
+      {/* <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      /> */}
+      
+      <img
+        src={image}
+        width='500'
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+
       {children}
     </Layout>
   )
