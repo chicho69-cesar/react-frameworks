9f92ee28cc3c378195d2d0fe713414395b798ef6
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-09-08 21:52:33 -0600

    feat(projects/expo-sticker-smash): Add render a static asset image for the image selector

--- projects/expo-sticker-smash/App.js
+++ projects/expo-sticker-smash/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+
+import ImageViewer from './components/ImageViewer'
+
+const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+
+      <StatusBar style='auto' />
     </View>
   )
 }
@@ -13,8 +20,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  }
 })
