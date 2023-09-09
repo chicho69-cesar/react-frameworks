d158c572790e89740183bcdb9ac3b8ccccaf41e3
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-09-09 14:16:27 -0600

    feat(projects/expo-sticker-smash): Add configuration for splash screen edge border color

--- projects/expo-sticker-smash/App.jsx
+++ projects/expo-sticker-smash/App.jsx
@@ -137,7 +137,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
 
-      <StatusBar style='auto' />
+      <StatusBar style='light' />
     </GestureHandlerRootView>
   )
 }
