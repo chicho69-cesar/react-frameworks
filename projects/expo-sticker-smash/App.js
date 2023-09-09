972f6a4ced1074fcc5a48d1eb03e0ea86cae50e9
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-09-08 22:03:48 -0600

    feat(projects/expo-sticker-smash): Add button component for the actions on app

--- projects/expo-sticker-smash/App.js
+++ projects/expo-sticker-smash/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
 
+import Button from './components/Button'
 import ImageViewer from './components/ImageViewer'
 
 const PlaceholderImage = require('./assets/images/background-image.png')
@@ -10,6 +11,11 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button label='Choose a photo' />
+        <Button label='Use this photo' />
       </View>
 
       <StatusBar style='auto' />
@@ -26,5 +32,9 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   }
 })
